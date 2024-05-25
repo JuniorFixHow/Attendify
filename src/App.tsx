431cb8b8f6dec9bd222d1570d88a4f0326163b67
JuniorFixHow <robertjuniorannan@gmail.com>
2024-05-25 15:35:21 +0100

    Changed route order

--- src/App.tsx
+++ src/App.tsx
@@ -12,50 +12,38 @@ type ProtectedRouteProps = {
 };
 
 function App() {
+  const { user } = useContext(AuthContext);
 
-  const {user} = useContext(AuthContext);
-  // console.log(user===null)
-
-  const ProtectedRoute=({children}:ProtectedRouteProps)=>{
-    if(!user){
-      return <Navigate to='/login' />
+  const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+    if (!user) {
+      return <Navigate to="/login" />;
+    } else {
+      return children;
     }
-    else{
-      return children
-    }
-  }
+  };
 
   return (
     <div className="app">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <ProtectedRoute>
-              <Register />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } />
+        <Route path="/register" element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        } />
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/sessions/:sessionId"
-          element={
-            <ProtectedRoute>
-              <Session />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/sessions/:sessionId" element={
+          <ProtectedRoute>
+            <Session />
+          </ProtectedRoute>
+        } />
       </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
