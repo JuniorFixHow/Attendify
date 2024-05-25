25a023ff5e0cfb5df2119069f0b9e746a9e2cce4
JuniorFixHow <robertjuniorannan@gmail.com>
2024-05-25 11:49:19 +0100

    Changed route settings

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useEffect, useReducer } from "react"
 import { AuthReducer } from "./AuthReducer";
+import { useNavigate } from "react-router-dom";
 // JSON.parse(localStorage.getItem("user")) || 
 type childrenProp={
     children:ReactNode
@@ -15,9 +16,13 @@ export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({children}:childrenProp)=>{
     const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+    const navigate = useNavigate();
 
     useEffect(()=>{
-        localStorage.setItem('user', JSON.stringify(state.user))
+        localStorage.setItem('user', JSON.stringify(state.user));
+        if(state.user){
+            navigate('/');
+        }
     }, [state.user])
 
     return(
--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,10 @@ import { AuthContextProvider } from './context/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthContextProvider>
         <App />
-      </BrowserRouter>
-    </AuthContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
